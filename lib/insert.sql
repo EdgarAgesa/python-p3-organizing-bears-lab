e6e1ce2aadb0cb5fbfafa13392434b3fa6e4d119
Edgar <edgarasige@gmail.com>
2024-06-05 14:15:33 +0300

    Completed assingments

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'White', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Brave', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'Golden', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'Green', 'Grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Brown', 'Energetic', 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (5, 'M', 'Black', 'Wild', 1);
